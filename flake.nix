dce45ba3cf9c0f662282b9aa30c5b36c93603b2e
nazozokc <nazozokc@users.noreply.github.com>
2026-09-11 13:23:35 +0900

    edit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "typescript-calendar";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_22
+            corepack
+            git
+          ];
+
+          shellHook = ''
+            corepack enable
+          '';
+        };
+      }
+    );
+}
